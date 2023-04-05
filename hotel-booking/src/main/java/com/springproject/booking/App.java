34b2da668c2c0bbebec840e4cc447752ca2f0456
liubakapower <liuboslavmotkov@gmail.com>
2023-04-05 12:03:20 +0300

    basicSetup
    added needed dependencies and main class(Already try to start server).

--- hotel-booking/src/main/java/com/springproject/booking/App.java
+++ hotel-booking/src/main/java/com/springproject/booking/App.java
@@ -1,13 +1,14 @@
 package com.springproject.booking;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ApplicationContext context = SpringApplication.run(App.class,args);
     }
 }
